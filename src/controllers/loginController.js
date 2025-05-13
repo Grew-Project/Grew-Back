7fd89a7e3d4f3aed582831330e4adcd83ee7a7f9
chemnida <chaemin9073@gmail.com>
2025-05-13 15:14:21 +0900

    Style : 세미콜론 제거

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,33 +1,33 @@
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 exports.loginUser = async (req, res) => {
-  const { user_id, password } = req.body;
+  const { user_id, password } = req.body
 
   try {
-    const user = await User.findOne({ user_id });
+    const user = await User.findOne({ user_id })
     if (!user) {
-      return res.status(401).json({ message: '존재하지 않는 아이디입니다.' });
+      return res.status(401).json({ message: '존재하지 않는 아이디입니다.' })
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password)
     if (!isMatch) {
-      return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+      return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' })
     }
 
     const token = jwt.sign(
       { user_id: user.user_id, nickname: user.nickname },
       process.env.JWT_SECRET,
       { expiresIn: process.env.JWT_EXPIRATION }
-    );
+    )
 
     res.status(200).json({
       message: '로그인 성공',
       token,
-    });
+    })
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: '서버 오류' });
+    console.error(err)
+    res.status(500).json({ message: '서버 오류' })
   }
-};
+}
--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
   user_id: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   nickname: { type: String, required: true, unique: true },
 
-  flower_count: { type: Number, default: 0 },  
-  leaf_count: { type: Number, default: 0 },    
-  created_at: { type: Date, default: Date.now }  
-});
+  flower_count: { type: Number, default: 0 },
+  leaf_count: { type: Number, default: 0 },
+  created_at: { type: Date, default: Date.now },
+})
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema)
