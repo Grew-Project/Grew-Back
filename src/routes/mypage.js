dfc815cd01c584e615f1424d4beb5d5a5b36baf8
udwns310 <uroasecret5@gmail.com>
2025-05-13 23:47:08 +0900

    Feat : 비밀번호 변경 API 구현
    - 비밀번호 변경 API 구현했습니다.

--- src/routes/mypage.js
+++ src/routes/mypage.js
@@ -4,7 +4,11 @@ const router = express.Router()
 const myAnswerRoutes = require('./mypage/myAnswerList')
 const myAnswerSearchRoutes = require('./mypage/myAnswerSearch')
 const myAnswerUpdateRoutes = require('./mypage/myAnswerUpdate')
+const myPasswordUpdateRoutes = require('./mypage/myPasswordUpdate')
+
 router.use('/my-answer-list', myAnswerRoutes)
 router.use('/my-answer-search', myAnswerSearchRoutes)
 router.use('/my-answer-update', myAnswerUpdateRoutes)
+router.use('/my-password-update', myPasswordUpdateRoutes)
+
 module.exports = router
