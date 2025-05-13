8455de0083af2597bd05939f5c8c6ee5f00d746f
udwns310 <uroasecret5@gmail.com>
2025-05-13 17:05:42 +0900

    Feat : 나무 이름 수정 API 구현
    - 나무 이름 수정 API를 구현했습니다.

--- src/routes/main.js
+++ src/routes/main.js
@@ -6,11 +6,14 @@ const answerCountRoutes = require('./main/answerCount')
 const flowerCountRoutes = require('./main/flowerCount')
 const leafCountRoutes = require('./main/leafCount')
 const treeNameRoutes = require('./main/treeName')
+const treeNameUpdateRoutes = require('./main/treeNameUpdate')
 
 router.use('/tree-info', treeInfoRoutes)
 router.use('/answer-count', answerCountRoutes)
 router.use('/flower-count', flowerCountRoutes)
 router.use('/leaf-count', leafCountRoutes)
 router.use('/tree-name', treeNameRoutes)
+router.use('/tree-name-update', treeNameUpdateRoutes)
 
 module.exports = router
+    
