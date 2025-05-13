77a3cd5c385000387c1eef34de2c93a8d726fa36
udwns310 <uroasecret5@gmail.com>
2025-05-13 14:51:22 +0900

    Style : Prettier 적용
    - db.js에 Prettier를 적용했습니다

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,8 @@ require('dotenv').config()
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI,{dbName: process.env.MONGODB_DB_NAME});
-    console.log('MongoDB connected');
-
+    await mongoose.connect(process.env.MONGODB_URI, { dbName: process.env.MONGODB_DB_NAME })
+    console.log('MongoDB connected')
   } catch (error) {
     console.error('MongoDB connection error:', error.message)
     process.exit(1)
