9c4af166fb1b31a1394bd51ff10a4b1f067b323d
chemnida <chaemin9073@gmail.com>
2025-05-13 17:49:06 +0900

    Feat : 전체, 감정 별 문답조회(커뮤니티)

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const treeInfoRoutes = require('./main/treeInfo')
 const answerCountRoutes = require('./main/answerCount')
+const answerSearchRoutes = require('./main/answerSearch')
 
-router.use('/tree-info', treeInfoRoutes) 
-router.use('/answer-count', answerCountRoutes) 
+router.use('/tree-info', treeInfoRoutes)
+router.use('/answer-count', answerCountRoutes)
+router.use('/answer-search', answerSearchRoutes)
 
 module.exports = router
