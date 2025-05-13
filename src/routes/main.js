40fe086bd24ae12f0398138f5acf27be9502c625
udwns310 <uroasecret5@gmail.com>
2025-05-13 17:31:23 +0900

    Feat : 잎사귀 조회 API 구현
    - 잎사귀 조회 API를 구현했습니다.

--- src/routes/main.js
+++ src/routes/main.js
@@ -7,6 +7,7 @@ const flowerCountRoutes = require('./main/flowerCount')
 const leafCountRoutes = require('./main/leafCount')
 const treeNameRoutes = require('./main/treeName')
 const treeNameUpdateRoutes = require('./main/treeNameUpdate')
+const leafContentRoutes = require('./main/leafContent')
 
 router.use('/tree-info', treeInfoRoutes)
 router.use('/answer-count', answerCountRoutes)
@@ -14,6 +15,6 @@ router.use('/flower-count', flowerCountRoutes)
 router.use('/leaf-count', leafCountRoutes)
 router.use('/tree-name', treeNameRoutes)
 router.use('/tree-name-update', treeNameUpdateRoutes)
+router.use('/leaf-content', leafContentRoutes)
 
 module.exports = router
-    
