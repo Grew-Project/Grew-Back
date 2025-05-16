7651e5a6aa17896a044d1cedb0c977de6cf32e15
udwns310 <uroasecret5@gmail.com>
2025-05-16 11:26:14 +0900

    Feat : 질문 API 추가
    - tree 컬렉션 answer_count 추가

--- src/routes/today/answerWrite.js
+++ src/routes/today/answerWrite.js
@@ -14,7 +14,13 @@ router.post('/', async (req, res) => {
       emotion_type,
       is_public,
     })
-
+    const tree = await Tree.findOne({ nickname })
+    if (tree) {
+      tree.answer_count += 1
+      await tree.save()
+    } else {
+      return res.status(404).json({ error: '해당 nickname에 대한 Tree 데이터가 없습니다.' })
+    }
     await newAnswer.save()
     res.status(201).json({ message: '답변 저장 완료' })
   } catch (err) {
